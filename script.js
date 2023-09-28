a652b66d2dc5342e72a5a00ad8d94b82c93cec3a
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-09-28 16:32:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+async function getconst() {
+  const url = 'https://moviesdatabase.p.rapidapi.com/actors';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'aaf412f001mshb16825a0d5065e6p181d8ajsn4a009ea4951a',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  const data = result.results;
+   const arrau = document.getElementById("div");
+arrau.innerHTML = data.map((list)=>list.primaryName);
+
+} catch (error) {
+	console.error(error);
+}
+}
+getconst()
